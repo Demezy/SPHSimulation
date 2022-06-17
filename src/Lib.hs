37d46fb3e14821f5a4f0549add013398448f1c3b
Roman Molochkov <molochkov2017@gmail.com>
2022-06-17 12:17:33 +0300

    feat: test the rendering of particle

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,8 @@ module Lib
     ) where
 
 import Graphics.Gloss
+import ParticleModule
+import Objects
 
 window :: Display
 window = InWindow "Nice Window" (200, 200) (10, 10)
@@ -13,5 +15,22 @@ background = white
 drawing :: Picture
 drawing = circle 80
 
+sampleParticle :: Particle
+sampleParticle = Particle {
+    position= (1, 1)
+  , velocity = (1, 1)
+  , mass = 42 
+  , config = undefined
+  }
+
+sampleParticle2 :: Particle
+sampleParticle2 = Particle {
+    position= (5, 5)
+  , velocity = (1, 1)
+  , mass = 42 
+  , config = undefined
+  }
+
+
 glossExample :: IO ()
-glossExample = display window background drawing
+glossExample = display window background ((renderParticleAt sampleParticle) <> (renderParticleAt sampleParticle2))
