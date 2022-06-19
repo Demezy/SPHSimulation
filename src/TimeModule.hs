dc425d18d9877a44ed8f00dd75b7134be18c160c
Demezy <work.mail.pub@gmail.com>
2022-06-19 15:22:51 +0300

    implement apply velocity function

--- src/TimeModule.hs
+++ src/TimeModule.hs
@@ -1,4 +1,15 @@
 module TimeModule where
 
+import Graphics.Gloss
+import Objects
 
-
+applyVelocity :: Particle -> Float -> Particle
+applyVelocity particle time =
+  particle
+    { position = (newX, newY)
+    }
+  where
+    newX = speedX * time + oldX
+    newY = speedY * time + oldY
+    (speedX, speedY) = velocity particle
+    (oldX, oldY) = position particle
