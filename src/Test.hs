3ae0f844f8fb300f3421ace5df0035dab4de430f
Roman Molochkov <molochkov2017@gmail.com>
2022-06-30 17:05:04 +0300

    refactor: refactor code of using Quadtrees

--- src/Test.hs
+++ src/Test.hs
@@ -1,8 +1,8 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Test where
 
 import Data.Monoid
-import Data.Foldable hiding (toList)
+import Data.Foldable
+    ( Foldable(length, foldMap, sum), all, concatMap )
 import Data.Maybe
 
 import Prelude hiding (Left, Right)
@@ -11,50 +11,33 @@ import Graphics.Gloss
 import Data.Bifunctor
 
 import Objects
+import Graphics.Gloss.Geometry.Line
 
 
-type P = (Double, Double)
-type Shape = P -> Double
+type Shape = Point -> Float
 
-circlee :: P -> Double -> Shape
+circlee :: Point -> Float -> Shape
 circlee (x0, y0) r (x, y) = sqrt ((x0 - x)^2 + (y0 - y)^2) - r
-
-left :: Double -> Shape
-left x0 (x, _) = x - x0
-
-right :: Double -> Shape
-right x0 (x, _) = x0 - x
-
-lower :: Double -> Shape
-lower y0 (_, y) = y - y0
-
-upper :: Double -> Shape
-upper y0 (_, y) = y0 - y
 
 (∪) :: Shape -> Shape -> Shape
 a ∪ b = \p -> min (a p) (b p)
 
-(∩) :: Shape -> Shape -> Shape
-a ∩ b  = \p -> max (a p) (b p)
-
-type Min = P
-type Max = P
+type Min = Point
+type Max = Point
 
 hi :: [Particle] -> Shape
 hi = r 
 
-r [x]      = circlee (realToFrac dx, realToFrac dy) 5
+r [x]      = circlee (dx, dy) 2
     where
         coordinate = position x
         dx = fst coordinate
         dy = snd coordinate
-
-r (x : xs) = circlee (realToFrac dx, realToFrac dy) 5 ∪ r xs
+r (x : xs) = circlee (dx, dy) 2 ∪ r xs
     where
         coordinate = position x
         dx = fst coordinate
         dy = snd coordinate
-
 
 --------------------------------------------------------------------------------
 
@@ -92,9 +75,10 @@ collapse shape leaf@(Leaf ((xmin, ymin), (xmax, ymax)))
       values = [shape (x, y) | x <- [xmin, xmax], y <- [ymin, ymax]]
 collapse shape (Root a b c d) =
     collapse' $ map (collapse shape) [a, b, c, d]
-    where collapse' [Empty, Empty, Empty, Empty] = Empty
-          collapse' [Full, Full, Full, Full] = Full
-          collapse' [q, r, s, t] = Root q r s t
+    where 
+        сollapse' [Empty, Empty, Empty, Empty] = Empty
+        collapse' [Full, Full, Full, Full] = Full
+        collapse' [q, r, s, t] = Root q r s t
 collapse _ t = t
 
 --------------------------------------------------------------------------------
@@ -103,27 +87,11 @@ instance Foldable Tree_ where
     foldMap f (Leaf a) = f a
     foldMap f (Root a b c d) = mconcat $ map (foldMap f) [a, b, c, d]
     foldMap _ _ = mempty
-
+    
 --------------------------------------------------------------------------------
 
--- foldTree tree quad root empty full leaf
-foldTree :: Monoid m => Tree -> Cell -> (Cell -> m) -> (Cell -> m)
-                                     -> (Cell -> m) -> (Cell -> m) -> m
-foldTree (Root a b c d) q@((xmin, ymin), (xmax, ymax)) root empty full leaf =
-    mconcat [root q, foldTree a ((xmin, ymin), (xmid, ymid)) root empty full leaf,
-                     foldTree b ((xmid, ymin), (xmax, ymid)) root empty full leaf,
-                     foldTree c ((xmin, ymid), (xmid, ymax)) root empty full leaf,
-                     foldTree d ((xmid, ymid), (xmax, ymax)) root empty full leaf]
-    where xmid = (xmin + xmax) / 2
-          ymid = (ymin + ymax) / 2
-
-foldTree Empty q _ f _ _ = f q
-foldTree Full  q _ _ f _ = f q
-foldTree (Leaf _) q _ _ _ f = f q
-
---------------------------------------------------------------------------------
-
-data Side = Upperr | Lowerr | Left | Right deriving Show
+data Side = Upperr | Lowerr | Left | Right 
+  deriving Show
 
 -- This lookup table takes a bitmask abcd and
 -- returns a list of edges between which we
@@ -148,127 +116,44 @@ lut = [[],                          -- 0000
 
 index :: Shape -> Cell -> Int
 index shape ((xmin, ymin), (xmax, ymax)) =
-    sum [if shape pt < 0 then 2^(3 - i) else 0 |
-         (pt, i) <- zip pts [0..]]
-    where pts = [(x,y) | y <- [ymin, ymax], x <- [xmin, xmax]]
+    sum [if shape pt < 0 
+         then 2^(3 - i) 
+         else 0 
+         | (pt, i) <- zip pts [0..]]
+      where 
+        pts = [(x,y) | y <- [ymin, ymax], x <- [xmin, xmax]]
 
 edges :: Shape -> Cell -> [(Side, Side)]
 edges shape c = lut !! index shape c
 
-pt :: Shape -> Cell -> Side -> P
+pt :: Shape -> Cell -> Side -> Point
 pt shape ((xmin, ymin), (xmax, ymax)) side =
-    case side of Left  -> zero shape (xmin, ymin) (xmin, ymax)
-                 Right -> zero shape (xmax, ymin) (xmax, ymax)
-                 Lowerr -> zero shape (xmin, ymin) (xmax, ymin)
-                 Upperr -> zero shape (xmin, ymax) (xmax, ymax)
+    case side of 
+        Left  -> zero shape (xmin, ymin) (xmin, ymax)
+        Right -> zero shape (xmax, ymin) (xmax, ymax)
+        Lowerr -> zero shape (xmin, ymin) (xmax, ymin)
+        Upperr -> zero shape (xmin, ymax) (xmax, ymax)
 
-zero :: Shape -> P -> P -> P
+zero :: Shape -> Point -> Point -> Point
 zero s a@(ax, ay) b@(bx, by)
     | s a >= 0 = zero s b a
-    | otherwise = zero' 1 1 10
+    | otherwise = zero' 0.5 0.25 10
     where pos f = (ax * (1-f) + bx * f, ay * (1-f) + by * f)
           zero' f step i
            | i == 0 = pos f
            | s (pos f) < 0 = zero' (f + step) (step / 2) (i - 1)
            | otherwise = zero' (f - step) (step / 2) (i - 1)
 
-type Edge = (P, P)
+type Edge = (Point, Point)
 
 contours :: Shape -> Cell -> [Edge]
 contours shape cell = [(pt' a, pt' b) |
                        (a, b) <- edges shape cell]
     where pt' = pt shape cell
 
---------------------------------------------------------------------------------
-merge :: Shape -> Tree -> Tree
-merge shape (Root a b c d) =
-    merge' a' b' c' d'
-    where [a', b', c', d'] = map (merge shape) [a, b, c, d]
-          merge' (Leaf (min, i)) (Leaf (q, r))
-                 (Leaf (s, t)) (Leaf (_, max)) =
-            let scores = map (score shape (min, max))
-                         [i, q, r, s, t]
-            in if all (< 0.001) scores
-               then Leaf (min, max)
-               else Root a' b' c' d'
-          merge' _ _ _ _ = Root a' b' c' d'
-merge _ t = t
+----------------------------------------------------------------------------
 
-interpolate :: Shape -> Cell -> P -> Double
-interpolate shape ((xmin, ymin), (xmax, ymax)) (x,y) =
-    let dx = (x - xmin) / (xmax - xmin)
-        dy = (y - ymin) / (ymax - ymin)
-        ab = shape (xmin, ymin) * (1 - dx) +
-             shape (xmax, ymin) * dx
-        cd = shape (xmin, ymax) * (1 - dx) +
-             shape (xmax, ymax) * dx
-    in ab * (1 - dy) + cd * dy
+listOfVectors particles =  (foldMap (contours (hi particles)) $ collapse (hi particles)$ buildTree (-500,-500) (500, 500) 9 )
 
-score :: Shape -> Cell -> P -> Double
-score shape cell pt = abs $ interpolate shape cell pt - shape pt
-
---------------------------------------------------------------------------------
-
-deriv :: Shape -> P -> P
-deriv shape (x,y) =
-    let epsilon = 0.001
-        dx = shape (x + epsilon, y) - shape (x - epsilon, y)
-        dy = shape (x, y + epsilon) - shape (x, y - epsilon)
-        len = sqrt $ dx^2 + dy^2
-    in (dx / len, dy / len)
-
-feature :: Shape -> Cell -> Maybe P
-feature shape cell =
-    if length pts_ >= 2 then
-        let pts = map fromTuple pts_
-            nms = map (fromTuple . deriv shape) pts_
-            center = sum pts / fromIntegral (length pts)
-
-            a = fromRows nms
-            b = col $ zipWith (\pt nm -> (pt - center) <·> nm) pts nms
-
-            p = center + head (toColumns $ linearSolveSVD a b)
-        in Just $ (\ [x, y] -> (x, y)) $ toList p
-    else Nothing
-    where pts_ = concatMap (\(a,b) -> [a,b]) $ contours shape cell
-          fromTuple = \(x,y) -> fromList [x,y]
-
---------------------------------------------------------------------------------
-
-dc :: Shape -> Tree -> [Edge]
-dc = faceProc
-
-faceProc :: Shape -> Tree -> [Edge]
-faceProc shape (Root a b c d) =
-    concatMap (faceProc shape) [a,b,c,d] ++
-    edgeProcH shape a b ++ edgeProcH shape c d ++
-    edgeProcV shape a c ++ edgeProcV shape b d
-faceProc _ _ = []
-
-edgeProcH :: Shape -> Tree -> Tree -> [Edge]
-edgeProcH shape (Leaf a) (Leaf b) = [(fromJust $ feature shape a,
-                                      fromJust $ feature shape b)]
-edgeProcH shape leaf@(Leaf _) (Root a _ c _) =
-    edgeProcH shape leaf a ++ edgeProcH shape leaf c
-edgeProcH shape (Root _ b _ d) leaf@(Leaf _) =
-    edgeProcH shape b leaf ++ edgeProcH shape d leaf
-edgeProcH shape (Root _ b _ d) (Root a _ c _) =
-    edgeProcH shape b a ++ edgeProcH shape d c
-edgeProcH _ _ _ = []
-
-edgeProcV :: Shape -> Tree -> Tree -> [Edge]
-edgeProcV shape (Leaf a) (Leaf b) = [(fromJust $ feature shape a,
-                                      fromJust $ feature shape b)]
-edgeProcV shape (Root _ _ c d) leaf@(Leaf _) =
-    edgeProcV shape c leaf ++ edgeProcV shape d leaf
-edgeProcV shape leaf@(Leaf _) (Root a b _ _) =
-    edgeProcV shape leaf a ++ edgeProcV shape leaf b
-edgeProcV shape (Root _ _ c d) (Root a b _ _) =
-    edgeProcV shape c a ++ edgeProcV shape d b
-edgeProcV _ _ _ = []
-
-f :: [Particle] -> [Edge]
-f particles = dc (hi particles) $ collapse (hi particles) $ merge (hi particles) $ buildTree (-500,-500) (500, 500) 11
-
-ff :: [Particle] -> [Picture]
-ff particles = map (\e -> line (bimap realToFrac realToFrac (fst e) : [bimap realToFrac realToFrac (snd e)])) (f particles)
+vectorsToPicture :: [Particle] -> [Picture]
+vectorsToPicture particles = map (\e -> line (bimap realToFrac realToFrac (fst e) : [bimap realToFrac realToFrac (snd e)])) (listOfVectors particles)
