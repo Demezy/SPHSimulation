4767d7d3d30fdd72183c1f61acb77defff7398c1
Roman Molochkov <molochkov2017@gmail.com>
2022-07-03 03:57:05 +0300

    refactor: change rendering function

--- src/RenderingOfUniverse.hs
+++ src/RenderingOfUniverse.hs
@@ -68,11 +68,11 @@ renderParticleAt particle = translate dx dy (renderParticle particle)
 
 -- | Render all Particles into Universe.
 renderParticles :: [Particle] -> Picture
-renderParticles particles = pictures (map renderParticleAt particles)
+--renderParticles particles = pictures (map renderParticleAt particles)
 
 -- | Render all Particles into Universe.
 --renderParticles :: [Particle] -> Picture
---renderParticles particles = scale 5.0 5.0 (pictures (vectorsToPicture particles))
+renderParticles particles = scale 5.0 5.0 (pictures (vectorsToPicture particles))
 
 -- | Render Solid by itself.
 renderWall :: Wall -> Picture
