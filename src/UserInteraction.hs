e01fbd97a4a31c879705b8e3d5089b84a887cbfe
Demezy <work.mail.pub@gmail.com>
2022-07-03 03:44:11 +0300

    add stub for walls drawing

--- src/UserInteraction.hs
+++ src/UserInteraction.hs
@@ -5,6 +5,7 @@ import Graphics.Gloss.Interface.Pure.Game
 import Objects
 import UsefulFunctions
 import SampleUnits
+import Data.Fixed (Uni)
 
 
 
@@ -15,7 +16,11 @@ handleEvent :: Event -> Universe -> Universe
 handleEvent (EventKey (SpecialKey KeyDown) Down _ _) universe = changeTimeMul (-1000) universe
 handleEvent (EventKey (SpecialKey KeyUp) Down _ _) universe = changeTimeMul 1000 universe
 -- handle mouse
+-- prefer explicitly use universe as argument, so wouldn't omit it!
 handleEvent (EventKey (MouseButton LeftButton) Down _ point) universe = addParticleToUniverse universe point
+handleEvent (EventKey (MouseButton RightButton) Down _ point) universe = startDrawWall universe point
+handleEvent (EventKey (MouseButton RightButton) Up _ point) universe = stopDrawWall universe point
+
 -- omit other Events
 handleEvent _ universe = universe
 
@@ -23,6 +28,7 @@ handleEvent _ universe = universe
 
 -- ==== Implementation ====
 
+-- == Add particles by LMB ==
 -- TODO determine parameters for users particles.
 -- For now use default one
 createParticle :: Point -- ^ 
@@ -34,3 +40,12 @@ addParticleToUniverse universe point = universe
   {
     fluid = createParticle point : fluid universe
   }
+
+
+
+-- == draw walls == 
+startDrawWall :: Universe -> Point -> Universe
+startDrawWall u _ = u
+
+stopDrawWall :: Universe -> Point -> Universe
+stopDrawWall u _ = u
